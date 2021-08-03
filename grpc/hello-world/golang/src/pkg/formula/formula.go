76ee3dbccd3eb84d68dbf5f09a6ae3333abd27c1
GuillaumeFalourd <guillaume.falourd@zup.com.br>
2021-08-03 09:38:42 -0300

    remove proto files
    Signed-off-by: GuillaumeFalourd <guillaume.falourd@zup.com.br>

--- grpc/hello-world/golang/src/pkg/formula/formula.go
+++ grpc/hello-world/golang/src/pkg/formula/formula.go
@@ -1,22 +1,3 @@
-/*
- *
- * Copyright 2015 gRPC authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
-
-// Package main implements a client for Greeter service.
 package formula
 
 import (
@@ -55,12 +36,15 @@ func (f Formula) Run() {
 	}
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
+
+	// Call SayHello method through gRPC.
 	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Greeting: %s", r.GetMessage())
 
+	// Call SayHelloAgain method through gRPC.
 	r, err = c.SayHelloAgain(ctx, &pb.HelloRequest{Name: name})
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
